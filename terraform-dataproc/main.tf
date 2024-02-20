381d5f2469353f9271dd749490f10d1870f01cc5
laszlo.agoston@telekom.com <laszlo.agoston@telekom.com>
2024-02-20 22:52:21 +0100

    added spark bigquery connectors to dataproc

--- terraform-dataproc/main.tf
+++ terraform-dataproc/main.tf
@@ -7,15 +7,26 @@ variable staging_bucket {}
 variable machine_type {}
 variable boot_disk_type {}
 
+#resource "google_compute_project_metadata" "default" {
+#  metadata = {
+#    bigquery-connector-version="1.2.0"
+#    spark-bigquery-connector-version="0.21.0"
+#    hive-bigquery-connector-version="2.0.3"
+#  }
+#}
+
 resource "google_dataproc_cluster" "small_cluster" {
   project = var.project
   name    = var.name
   provider = google-beta
   region  = var.region
+#  depends_on = [
+#    google_compute_project_metadata.default
+#  ]  
   
   cluster_config {
     staging_bucket = var.staging_bucket
-
+    
     master_config {
       machine_type = var.machine_type
       disk_config {
@@ -38,6 +49,18 @@ resource "google_dataproc_cluster" "small_cluster" {
     endpoint_config {
       enable_http_port_access = "true"
     }
+    initialization_action {
+    script = "gs://goog-dataproc-initialization-actions-europe-west3/connectors/connectors.sh"
+    timeout_sec = 500
+    }
+    gce_cluster_config {
+      metadata = {
+        bigquery-connector-version="1.2.0"
+        spark-bigquery-connector-version="0.21.0"
+        hive-bigquery-connector-version="2.0.3"
+      }
+    }
+
   }
 }
 
